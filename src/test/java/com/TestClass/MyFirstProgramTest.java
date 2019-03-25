aa7e7003ffcc3efdab1e527cd1a6e12acbd8045a
Your Name <selvakumar>
2019-03-25 20:11:41 +0530

    Adding second class into GIT

--- src/test/java/com/TestClass/MyFirstProgramTest.java
+++ src/test/java/com/TestClass/MyFirstProgramTest.java
@@ -1,6 +1,10 @@
 package com.TestClass;
 
 
+import java.awt.AWTException;
+import java.awt.Robot;
+import java.awt.event.KeyEvent;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
@@ -8,23 +12,21 @@ import org.testng.annotations.Test;
 
 public class MyFirstProgramTest {
 	
-	@Test
-	public void TestMethod() {
+	public WebDriver driver;
+	public Robot robot;
+	public void TestMethod() throws AWTException {
 	//public static void main(String arg[]) {	
 		System.out.println("Welcome to DevOps");
 		
+		robot = new Robot();
+		
 		System.setProperty("webdriver.chrome.driver", "C:\\WebDrivers\\chromedriver_win32\\chromedriver.exe");
 		
-		WebDriver driver= new ChromeDriver();
+		driver= new ChromeDriver();
 		driver.manage().deleteAllCookies();
 		driver.manage().window().maximize();
 		System.out.println("ChromeDriver is successfully launched");
-		driver.get("https://www.google.com/");
-		System.out.println("You are in Given webPage");
-		
-		String title=driver.getTitle();
-		System.out.println("Title is " +title);
-		driver.quit();
+
 	}
 	
 
---
+++ src/test/java/com/TestClass/SecondProgramTest.java
@@ -0,0 +1,31 @@
+package com.TestClass;
+
+import java.awt.AWTException;
+import java.awt.event.KeyEvent;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.Test;
+
+public class SecondProgramTest extends MyFirstProgramTest {
+
+	@Test
+	public void googleTest() throws AWTException, InterruptedException {
+		TestMethod();
+		driver.get("https://www.google.com/");
+		System.out.println("You are in Given webPage");
+		
+		String title=driver.getTitle();
+		System.out.println("Title is " +title);
+		driver.findElement(By.xpath("//input[@class='gLFyf gsfi']")).sendKeys("Facebook");
+		robot.keyPress(KeyEvent.VK_ENTER);
+		robot.keyRelease(KeyEvent.VK_ENTER);
+		Thread.sleep(3000);
+		String SecondPageTitle=driver.getTitle();
+		System.out.println("Second Page title is " +SecondPageTitle);
+		driver.quit();
+		
+	}
+	
+	
+}
